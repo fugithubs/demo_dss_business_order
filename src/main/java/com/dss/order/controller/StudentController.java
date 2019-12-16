170f1dfcfdb71bc34e3889ddf5cf0e7d21465050
fugithubs <1091565452@qq.com>
2019-12-16 18:28:50 +0800

    增加练习分页查询方法，调整RequestMapping为对应的GetMapping,PostMapping

--- src/main/java/com/dss/order/controller/StudentController.java
+++ src/main/java/com/dss/order/controller/StudentController.java
@@ -2,16 +2,18 @@ package com.dss.order.controller;
 
 import com.dss.order.domain.Student;
 import com.dss.order.service.StudentService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
-import kafka.utils.Json;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import javax.xml.ws.Response;
 import java.util.List;
 
 /**
@@ -26,19 +28,43 @@ import java.util.List;
 @Api(value = "StudentController", description = "测试相关API")
 public class StudentController {
 
+    private final static Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @Resource
     private StudentService studentService;
 
-    @RequestMapping(name = "/all", method = RequestMethod.GET)
+    @GetMapping("/all")
     public ResponseEntity<JsonResult> getAllSutdent() {
+        logger.info("<<执行/student/all方法开始>>");
         JsonResult result = new JsonResult();
         List<Student> students = studentService.selectByExample(null);
         if (students != null && students.size() > 0) {
             result.setStatus(JsonResult.RESULT_OK);
             result.setResult(students);
+            logger.info("<<执行/student/all方法返回数据>>");
             return ResponseEntity.ok(result);
         }
         result.setStatus(JsonResult.RESULT_FAIL);
+        logger.error("<<执行/student/all方法无数据>>");
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 分页查询学生数据
+     * @param page
+     * @return
+     */
+    @GetMapping("/page")
+    public ResponseEntity<JsonResult> getPageInfo(@RequestParam("page") Integer page){
+        if (page==null) {
+            page = 1;
+        }
+        PageHelper.startPage(page,3);
+        List<Student> students = studentService.selectByExample(null);
+        PageInfo<Student> pageInfo = new PageInfo<>(students);
+        JsonResult result = new JsonResult();
+        result.setStatus(JsonResult.RESULT_OK);
+        result.setResult(pageInfo);
         return ResponseEntity.ok(result);
     }
 }
